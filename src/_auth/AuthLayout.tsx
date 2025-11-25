91fd90ed9dae8517c0662ace4898b2e0909b4960
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-11-25 11:05:17 +0545

    fix: redirect according to authentication

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,24 +1,24 @@
+import { useUserContext } from '@/context/AuthContext'
 import React from 'react'
-import { Navigate,Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 const AuthLayout = () => {
-const isAuthenticated=false
-
+  const { isAuthenticated } = useUserContext();
 
   return (
     <>
-    {isAuthenticated ? (
-      <Navigate to='/' />
-    ):(
-      <>
-      <section className='flex flex-1 justify-center items-center flex-col py-10'>
-        <Outlet/>
-        </section>
-        <img 
-        src='/assets/images/side-img.svg'
-        alt="logo"
-        className='hiiden xl:block h-screen w-1/2 object-cover bg-no-repeat'/></>
-        
-    )}
+      {isAuthenticated ? (
+        <Navigate to='/' />
+      ) : (
+        <>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
+            <Outlet />
+          </section>
+          <img
+            src='/assets/images/side-img.svg'
+            alt="logo"
+            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
+        </>
+      )}
     </>
   )
 }
